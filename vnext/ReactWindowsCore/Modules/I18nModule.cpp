d4693d1c2a5bfa7214e60bf18f382a9c18523fbf
Aaron Schultz <aschultz@microsoft.com>
2020-02-11 18:00:42 -0800

    format

--- vnext/ReactWindowsCore/Modules/I18nModule.cpp
+++ vnext/ReactWindowsCore/Modules/I18nModule.cpp
@@ -18,28 +18,25 @@ std::map<std::string, folly::dynamic> I18nModule::getConstants() {
 }
 
 std::vector<facebook::xplat::module::CxxModule::Method> I18nModule::getMethods() {
-  return {
-    Method(
-      "allowRTL",
-      [this](folly::dynamic args) {
-        if (args.size() > 0 && args[0].isBool()) {
-          m_module->allowRTL(args[0].asBool());
-        }
-      }),
-    Method(
-      "forceRTL",
-      [this](folly::dynamic args) {
-        if (args.size() > 0 && args[0].isBool()) {
-          m_module->forceRTL(args[0].asBool());
-        }
-      }),
-    Method("swapLeftAndRightInRTL",
-      [this](folly::dynamic args) {
-        if (args.size() > 0 && args[0].isBool()) {
-          m_module->swapLeftAndRightInRTL(args[0].asBool());
-        }
-      })
-  };
+  return {Method(
+              "allowRTL",
+              [this](folly::dynamic args) {
+                if (args.size() > 0 && args[0].isBool()) {
+                  m_module->allowRTL(args[0].asBool());
+                }
+              }),
+          Method(
+              "forceRTL",
+              [this](folly::dynamic args) {
+                if (args.size() > 0 && args[0].isBool()) {
+                  m_module->forceRTL(args[0].asBool());
+                }
+              }),
+          Method("swapLeftAndRightInRTL", [this](folly::dynamic args) {
+            if (args.size() > 0 && args[0].isBool()) {
+              m_module->swapLeftAndRightInRTL(args[0].asBool());
+            }
+          })};
 }
 
 std::unique_ptr<facebook::xplat::module::CxxModule> createI18nModule(std::unique_ptr<II18nModule> module) {
